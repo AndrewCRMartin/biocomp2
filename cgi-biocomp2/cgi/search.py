4fc10b9283ffa92865307157bba1655415a0a45c
Andrew Martin <Andrew C.R. Martin>
2019-07-31 17:54:13 +0100

    Added dummy search.py code

---
+++ cgi-biocomp2/cgi/search.py
@@ -0,0 +1,30 @@
+#!/usr/bin/python3
+"""
+...Comment header goes here...
+
+This CGI script obtains all the entries from the BL layer and formats them for 
+HTML display as a table
+"""
+
+# Add the bl sub-directory to the module path
+# and the directory above to import the config file
+import sys
+sys.path.insert(0, "../bl/")
+sys.path.insert(0, "../")
+
+import blapi      # Import the Business Logic API
+import htmlutils  # Import HTML utilities
+import config     # Import configuration information (e.g. URLs)
+
+html    = htmlutils.header()
+
+html += "<h1>Dummy search code</h1>\n"
+html += "      <ul>\n"
+html += "        <li>Some search result</li>\n"
+html += "      </ul>\n"
+html += htmlutils.footer()
+
+print(html)
+
+
+
