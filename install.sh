30b80657aa380184a6c704b2ad10d859b935b5bd
AndrewCRMartin <andrew@bioinf.org.uk>
2019-08-01 17:22:15 +0100

    Updated install.sh script

--- install.sh
+++ install.sh
@@ -1,10 +1,8 @@
 html=/var/www/html/biocomp2
 cgi=/var/www/cgi-bin/biocomp2
 
-sudo mkdir -p $html
-sudo chown andrew:andrew $html
-sudo mkdir -p $cgi
-sudo chown andrew:andrew $cgi
+mkdir -p $html
+mkdir -p $cgi
 
 cp -R html/* $html
 cp -R cgi-biocomp2/* $cgi
